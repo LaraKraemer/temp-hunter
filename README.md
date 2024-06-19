3586130ec606d6c50bd0a830db45f5a4d8f05050
LaraKraemer <51157020+LaraKraemer@users.noreply.github.com>
2024-06-19 11:08:39 +0200

    Store data in sqlite database

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Scraping temperature data 
 This was a project from my Udemy bootcamp where I scraped temperature data. 
-The lectures focused on learning how to scrape and store the data.
+The lectures focused on learning how to scrape and store the data in a SQLite database.
 
 ## How to work with this repository
 1. Clone the repository
@@ -9,7 +9,9 @@ The lectures focused on learning how to scrape and store the data.
 4. Install venv: `python3 -m venv venv`
 5. Run venv: `source venv/bin/activate`
 6. Add yaml file : `extract.yaml` 
-7. Add storing file: `data.txt`
+7. Install:  `sqlite3`
+8. Create respective: `database and table`
+9. Connect database in your codebase: `connection = sqlite3.connect("FILE")`
 8. Write more code, write the tests, and have fun! 🎉
 
 
---
+++ main.py
@@ -0,0 +1,39 @@
+import sqlite3
+import requests
+import selectorlib
+from src import creds
+from datetime import datetime
+import time
+
+date = datetime.today().strftime("%d/%m/%Y %H:%M:%S")
+connection = sqlite3.connect("files/data.db")
+cursor = connection.cursor()
+
+def scrape(url):
+    """Scrape the page source from the URL"""
+    response = requests.get(url)
+    source = response.text
+    return source
+
+
+def extract(source):
+    """extract temp data"""
+    extractor = selectorlib.Extractor.from_yaml_file("files/extract.yaml")
+    value = extractor.extract(source)["temperature"]
+    return value
+
+
+def store(date, extracted):
+    """store date, temp data"""
+    date = datetime.today().strftime("%d/%m/%Y %H:%M:%S")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO temp VALUES(?,?)", (date, extracted))
+    connection.commit()
+
+
+if __name__ == "__main__":
+    while True:
+        scraped = scrape(creds.URL)
+        temperature = extract(scraped)
+        store(date, temperature)
+    time.sleep(2)
